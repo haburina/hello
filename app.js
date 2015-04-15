61efc820f4089ff30dfc157b5dd4862fdcaef5d6
haburina <alena.khaburina@gmail.com>
2015-04-14 22:00:30 -0700

    var

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ var main = function() {
     var postLength = $(this).val().length;
     var charactersLeft = 140 - postLength;
     $('.counter').text(charactersLeft);
+  
+    if(charactersLeft < 0) {
+      $('.btn').addClass('disabled');
+    }
+    else if(charactersLeft == 140) {
+      $('.btn').addClass('disabled');
+    }
+    else {
+      $('.btn').removeClass('disabled');
+    }    
+
   });
-}  
 
+ };  
 
+$('.btn').addClass('disabled');
 $(document).ready(main);

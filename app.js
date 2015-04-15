edaf000353f1370bc8f20478520f579af0abf4ec
haburina <alena.khaburina@gmail.com>
2015-04-14 19:20:40 -0700

    var

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ var main = function() {
     if(event.which === 111) {
       $('.current').children('.description').toggle();
     }
+    
+    else if(event.which === 110) {
+      var currentArticle = $('.current');
+      var nextArticle = currentArticle.next();
+      
+      currentArticle.removeClass('current');
+      nextArticle.addClass('current');
+    }
   });
 }
 
